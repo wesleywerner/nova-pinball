16f9d8964e764f70c1c5843bb50b84ca9fbeac64
Wesley <wesley.werner@gmail.com>
2015-11-11 21:22:07 +0200

    Implement music playlist play and pause functions.

--- music/playlist.lua
+++ music/playlist.lua
@@ -20,39 +20,101 @@
 
 local playlist = {}
 
+playlist.turnedOn = false
+
+playlist.trackIndex = 1
+
+playlist.source = nil
+
+playlist.playedCount = 0
+
 playlist.tracks = {
     {
         file="music/nexus_x.it",
         credit="Nexus, by Xaser",
+        url="http://modarchive.org/index.php?request=view_by_moduleid&query=50384",
         loop=2,
-        volume=0.4,
+        volume=0.6,
     },
     {
         file="music/nosotr_x.it",
         credit="Nosotros, by Xaser",
+        url="http://modarchive.org/index.php?request=view_by_moduleid&query=51677",
         loop=2,
-        volume=0.4,
+        volume=0.6,
     },
     {
         file="music/tarvoi_x.it",
         credit="Void of Space, by Xaser",
+        url="http://modarchive.org/index.php?request=view_by_moduleid&query=160395",
         loop=4,
-        volume=0.4,
+        volume=0.6,
     },
     {
         file="music/voxel_x.it",
         credit="voxelbuffer, by Xaser",
+        url="http://modarchive.org/index.php?request=view_by_moduleid&query=162116",
         loop=2,
-        volume=0.4,
+        volume=0.6,
     },
     {
         file="music/zeroph_x.it",
         credit="Zerophine, by Xaser",
+        url="http://modarchive.org/index.php?request=view_by_moduleid&query=160368",
         loop=2,
-        volume=0.4,
+        volume=0.6,
     },
 }
 
+function playlist:load()
 
+end
+
+function playlist:play()
+    playlist.turnedOn = true
+end
+
+function playlist:pauseUnpause()
+    if (playlist.turnedOn and playlist.source) then
+        if (playlist.source:isPaused()) then
+            playlist.source:resume()
+        else
+            playlist.source:pause()
+        end
+    end
+end
+
+function playlist:stop()
+    playlist.turnedOn = false
+    if (playlist.source) then
+        love.audio.stop(playlist.source)
+        playlist.source = nil
+    end
+end
+
+function playlist:update(dt)
+    if (playlist.turnedOn) then
+        -- next track
+        if (not playlist.source) then
+            local track = playlist.tracks[playlist.trackIndex]
+            -- Loop the same track or skip to the next track
+            playlist.playedCount = playlist.playedCount + 1
+            if (playlist.playedCount > track.loop) then
+                playlist.playedCount = 1
+                playlist.trackIndex = playlist.trackIndex + 1
+                if (playlist.trackIndex > #playlist.tracks) then playlist.trackIndex = 1 end
+            end
+            local track = playlist.tracks[playlist.trackIndex]
+            playlist.source = love.audio.newSource(track.file, "stream")
+            playlist.source:setVolume(track.volume)
+            love.audio.play(playlist.source)
+        else
+        -- Track is finished playing
+            if (playlist.source:isStopped()) then
+                playlist.source = nil
+            end
+        end
+    end
+end
 
 return playlist
