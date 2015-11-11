ddc371dbff96c85fcb14cdc38d3907cc218904a3
Wesley <wesley.werner@gmail.com>
2015-11-11 13:13:10 +0200

    Add a release script (depends on love-release)

---
+++ dev/release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo -e "\nBuild Release"
+echo "--------------"
+read -p "Version: " VER
+if [ -z $VER ]; then
+    echo "No version given. Exiting."
+    exit 1
+fi
+
+# The love version to build against
+LOVER="0.9.1"
+
+# Win32
+love-release -W32 -t "nova-pinball-$VER" -l $LOVER -v $VER -x image-sources\/\* -x dev\/\* -x nova-pinball-engine\/editor\/\*
