b062c0cd2f90d3831f0e6d8007a5711d3a1f1d5a
Wesley <wesley.werner@gmail.com>
2015-12-10 08:37:00 +0200

    Update version

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@
 -- Written by Wesley "keyboard monkey" Werner 2015
 -- https://github.com/wesleywerner/
 
-VERSION = "0.2.2.1"
+VERSION = "0.2.2.2"
 DEBUG = false
 spriteManager = require("modules.sprite-state-manager")
 stateManager = require ("modules.states")
